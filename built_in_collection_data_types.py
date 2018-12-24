2f5b8b8b7a5f68cbc95e56a17a65fe5dd0f83230
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-23 18:20:41 -0800

    working wit range

--- built_in_collection_data_types.py
+++ built_in_collection_data_types.py
@@ -1,27 +1,30 @@
-my_list = [1024, 3, True, 6.5]
-my_list.append(False)
-print(my_list)
-
-my_list.insert(2,4.5)
-print(my_list)
-
-print(my_list.pop())
-print(my_list)
-
-print(my_list.pop(1))
-print(my_list)
-
-# print(my_list.pop(2))
+# my_list = [1024, 3, True, 6.5]
+# my_list.append(False)
+# print(my_list)
+#
+# my_list.insert(2,4.5)
+# print(my_list)
+#
+# print(my_list.pop())
+# print(my_list)
+#
+# print(my_list.pop(1))
+# print(my_list)
+#
+# # print(my_list.pop(2))
+# # print(my_list)
+#
+# my_list.sort()
+# print(my_list)
+#
+# my_list.reverse()
+# print(my_list)
+#
+# print(my_list.count(6.5))
+# print(my_list.index(4.5))
+#
+# del my_list[0]
 # print(my_list)
 
-my_list.sort()
-print(my_list)
-
-my_list.reverse()
-print(my_list)
-
-print(my_list.count(6.5))
-print(my_list.index(4.5))
-
-del my_list[0]
-print(my_list)
+range(10)
+print(range)
