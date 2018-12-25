e25aea691fee311284203649215558b4065f5ff6
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-25 12:08:05 -0800

    calculating the running time

--- chapter_2.2.py
+++ chapter_2.2.py
@@ -1,10 +1,33 @@
-import time 
+import time
 
-def sum_of_n(n):
-    the_sum = 0
-    for i in range(1, n+1):
-        the_sum = the_sum + i
+# def sum_of_n(n):
+#     the_sum = 0
+#     for i in range(1, n+1):
+#         the_sum = the_sum + i
+#
+#     return the_sum
+#
+# print(sum_of_n(10))
 
-    return the_sum
+# def sum_of_n2(n):
+#     start_time = time.time()
+#
+#     the_sum = 0
+#     for i in range(1, n+1):
+#         the_sum = the_sum + i
+#
+#     end_time = time.time()
+#
+#     return the_sum, end_time-start_time
+#
+# print(sum_of_n2(100000000000))
 
-print(sum_of_n(10))
+
+def sum_of_n3(n):
+    start_time = time.time()
+    sum_number = (n*(n+1))/2
+    end_time = time.time()
+    return sum_number, end_time-start_time
+
+
+print(sum_of_n3(900))
