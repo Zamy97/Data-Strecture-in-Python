bba546c1ba4337ad0ffcae00386895a1b1f4aa90
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-27 20:47:04 -0800

    working on big O

--- Chapter-2/self_check_1.py
+++ Chapter-2/self_check_1.py
@@ -1,7 +1,7 @@
 ## Write two Python functions to find the minimum number in a list. The first function should compare each number to every other number on the list. O(n2). The second function should be linear O(n)
 
 def find_min_value(a_list):
-    # overall_min = a_list[0]
+    overall_min = a_list[0]
     for i in a_list:
         is_smallest = True
         for j in a_list:
