8e992a173fdbb29f8457996af1420954d1d220a8
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-27 21:18:36 -0800

    testing the speed of the algorithm

--- Chapter-2/self_check_1.py
+++ Chapter-2/self_check_1.py
@@ -2,7 +2,10 @@
 import time
 from random import randrange
 
-def find_min_value(a_list):
+def find_min_value_1(a_list):
+    """ Finds the minimum value in a given list using the O(n^2)
+        Uses Two nested loops
+    """
     overall_min = a_list[0]
     for i in a_list:
         is_smallest = True
@@ -13,7 +16,34 @@ def find_min_value(a_list):
             overall_min = i
     return overall_min
 
-print(find_min_value([2,5,1,7,9,9,5,2,1,9,0,3,3,-9]))
+
+def find_min_value_2(a_list):
+    """ Finds the minimum value in a  given list using linear approach
+        Uses Linear approach
+    """
+    # Checks number against the list to see if it's the smallest or not
+    min_so_far = a_list[0]
+    for i in a_list:
+         if i < min_so_far:
+             min_so_far = i
+    return min_so_far
+
+print(find_min_value_1([2,5,1,7,9,9,5,2,1,9,0,3,3,-9]))
+print(find_min_value_2([9,4,2,-5,-1,6,-2,-8,8,1,8,0,-767]))
 
 
-for list_size in range(1000,10001, 1000)
+# Checks the speed of the first approach. Kind of slow and takes a while to execute
+for list_size in range(1000,10001, 1000):
+    a_list = [randrange(100000) for x in range(list_size)]
+    start_time = time.time()
+    print(find_min_value(a_list))
+    end_time = time.time()
+    print("Size: %d time: %f" % (list_size, end_time-start_time))
+
+# Checks the speed of the second approach (O(n)). Doesn't take that long to execute.
+for list_size in range(1000,10001, 1000):
+    a_list = [randrange(100000) for x in range(list_size)]
+    start_time = time.time()
+    print(find_min_value(a_list))
+    end_time = time.time()
+    print("Size: %d time: %f" % (list_size, end_time-start_time))
