9eb746c3438c9cfed6b5fbe8a267fb81952dacfa
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-29 23:12:27 -0800

    working with timeit method

---
+++ Chapter-2/listing_5.py
@@ -0,0 +1,15 @@
+import timeit
+x = list(range(2000000))
+
+pop_start= timeit.Timer("x.pop(0)", "from __main__ import x")
+
+
+pop_end = timeit.Timer("x.pop()", "from __main__ import x")
+
+print("         pop(0)            pop()")
+for i in range(1000000, 10000001, 1000000):
+    x = list(range(i))
+    pt = pop_end.timeit(number = 1000)
+    x = list(range(i))
+    pz=pop_start.timeit(number = 1000)
+    print("%15.5f, %15.5f" %(pz, pt))
