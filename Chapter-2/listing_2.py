a78a448f7c3b551861fd4d865a2f5256bf13d61d
Akhtar Zaman <auz.zamy@gmail.com>
2019-06-08 22:05:15 -0400

    working on chapter 2

---
+++ Chapter-2/listing_2.py
@@ -0,0 +1,19 @@
+# a = 5
+# b = 6
+# c = 10
+# for i in range(n):
+#     for j in range(n):
+#         x = i * i
+#         y = j * j
+#         z = i * j
+# for k in range(n):
+#     w = a*k + 45
+#     v = b*b
+# d = 33
+
+## self check
+some_list = list(range(9))
+def min_number(some_list):
+    for a_number in some_list:
+        return a_number
+min_number(some_list)
