4a7e1e36b68b688b035221a540a66730a2339330
Akhtar Zaman <auz.zamy@gmail.com>
2019-06-10 11:13:31 -0400

    working on chapter 2

--- Chapter-2/listing_2.py
+++ Chapter-2/listing_2.py
@@ -14,6 +14,11 @@
 ## self check
 some_list = list(range(9))
 def min_number(some_list):
+    overall_min = some_list[0]
     for a_number in some_list:
+        is_smallest = True
+        for j in some_list:
+            if i > j:
+                is_smallest = False
         return a_number
 min_number(some_list)
