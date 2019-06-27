632651b7c36c8bc57529c23fde1dccb1e0e892e1
Akhtar Zaman <auz.zamy@gmail.com>
2019-06-27 01:20:38 -0400

    learning to implement stack

--- Chapter-3/3.5_stack.py
+++ Chapter-3/3.5_stack.py
@@ -1,3 +1,4 @@
+# from pythonds.basic.stack import Stack
 class Stack():
     def __init__(self):
         self.items = []
@@ -7,7 +8,7 @@ class Stack():
         return self.items == []
 
     # Adds a new item to the tp of the stack. Return nothing.
-    def push(self):
+    def push(self, item):
         self.items.append(item)
 
     # Removes and return the top item from the stack
@@ -21,3 +22,42 @@ class Stack():
     #Retuns the size of the Stack
     def size(self):
         return len(self.items)
+
+
+
+###################### Different implementation of stack #######################
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.insert(0,tiem)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peeks(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+#################### Testing the Stack Class ####################
+
+
+example_stack = Stack()
+# print(example_stack.isEmpty())
+example_stack.push(4)
+example_stack.push("Dog")
+print(example_stack.peek())
+print(example_stack.isEmpty())
+example_stack.push(True)
+print(exam.size())
+print(example_stack.isEmpty())
+example_stack.push(8.4)
+print(example_stack.pop())
+print(example_stack.pop())
+print(example_stack.size())
