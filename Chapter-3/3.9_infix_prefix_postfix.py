74763762d0d2fb254fb418acbaa72c7536ee134b
Aktar Zaman <zamy@Aktars-MBP.home>
2019-07-09 11:50:09 -0400

    working on infix to postfix conversion

---
+++ Chapter-3/3.9_infix_prefix_postfix.py
@@ -0,0 +1,13 @@
+ from pythonds.basic import Stack
+
+def infix_to_postfix(infix_expr):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    op_stack = Stack()
+    postfix_list = []
+    token_list = infix_expr.split()
+    
